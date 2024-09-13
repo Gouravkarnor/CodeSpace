36257e2dd4ad433116d7e11f335065c681febf99
Gouravkarnor <gaurav9karnor@gmail.com>
2024-09-13 19:26:44 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,9 +9,4 @@ export default defineConfig({
       "@emotion/styled": "@emotion/styled",
     },
   },
-  build: {
-    rollupOptions: {
-      external: ["mongoose"], // Ignore 'mongoose' during the build
-    },
-  },
 });
